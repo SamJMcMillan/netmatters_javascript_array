6c28762f3d79e1b99275ae4a1b21dec92fad972a
SamJMcMillan <sam.ja.mcmillan@gmail.com>
2022-11-21 16:44:57 +0000

    HTML / CSS additions

---
+++ js/email-submission.js
@@ -0,0 +1,11 @@
+const emailPopup = document.getElementById("email-error");
+
+emailPopup.display = "none";
+
+function validateForm() {
+  let emailPattern =
+    /^(([^<>()[]\.,;:\s@"]+(.[^<>()[]\.,;:\s@"]+)*)|(".+"))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/;
+  let emailInput = $(".email-input").val().trim();
+  if (!emailPattern.test(emailInput)) {
+  }
+}
