2fa819789d3958345a53e8e1ac2bc15ae839904d
SamJMcMillan <sam.ja.mcmillan@gmail.com>
2022-11-22 13:21:44 +0000

    JS and favicon fixes

--- assets/javascript/emailSub.js
+++ assets/javascript/emailSub.js
@@ -1,4 +1,5 @@
 const emailPopup = document.getElementById("email-error");
+var currentImage = document.getElementById("current-image");
 
 emailPopup.style.display = "none";
 
@@ -8,5 +9,12 @@ function validateForm() {
   if (!emailPattern.test(emailInput)) {
     $(emailPopup).slideDown(400).delay(2000).slideUp(400);
     return false;
+  } else {
+    $(imageArray).append(currentImage);
+    return true;
   }
 }
+
+// identify the current image
+// append it to the array
+// have a slider display images in array
